a61bf6ef0d7ab19a944c73a28bffbe1c580d3c2b
Mark Smith <mark.smith@practicalpoetry.co.uk>
2013-05-02 12:08:06 +0100

    Added branch point for starting webserver.

--- capo.py
+++ capo.py
@@ -7,19 +7,30 @@ Capo is your friendly handicap-race manager.
 
 from __future__ import absolute_import
 
+import sys
+import argparse
 from capolib.console import CapoCmd
 
 
-def main():
+def main(argv=sys.argv[1:]):
     """
     Entry point for Capo.
     """
-    driver = CapoCmd('capo.sqlite')
-    try:
-        driver.cmdloop()
-    except KeyboardInterrupt:
-        print
+    arg_parser = argparse.ArgumentParser(description="Run Capo")
+    arg_parser.add_argument('command', default='console', nargs='?', choices=['web', 'console'])
+    args = arg_parser.parse_args(argv)
+
+    if args.command == 'web':
+        print >>sys.stderr, 'Web support not yet implemented.'
+        return -1
+    elif args.command == 'console':
+        driver = CapoCmd('capo.sqlite')
+        try:
+            driver.cmdloop()
+        except KeyboardInterrupt:
+            print
+        return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
